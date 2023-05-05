b243cc120723cff723a977fab87b1f6eb3f9736e
ElenaIvanovaIQsmart <smart.maks@bk.ru>
2023-05-05 13:58:35 +0300

    First commit!

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    description = RichTextUploadingField()
+
+    def __str__(self):
+        return self.title
+

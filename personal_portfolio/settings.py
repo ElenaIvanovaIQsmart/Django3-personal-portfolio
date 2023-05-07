618922f8af71a002893c16ddce9ac76fb3ea8840
Elena <IQsmart@4146c987a9a6>
2023-05-07 16:34:26 +0000

    changedsettings file on web

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--2+4i0634vqw!4x$%a&0fflu8!%e8lqp_mr^21n^6%=1qf%d-q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iqsmart.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 CKEDITOR_UPLOAD_PATH = "uploads/"
